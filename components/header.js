924d2e08f665441d77159f87c7ebbddc7f9cc8fc
Lewis Inches <incheslewis@gmail.com>
2023-07-15 22:02:48 +0100

    remove collections from header

--- components/header.js
+++ components/header.js
@@ -3,17 +3,17 @@ import styles from './css-modules/header.module.css'
 import { useEffect, useState } from 'react'
 
 export default function Header () {
-    const [data, setCollections] = useState([]);
+    // const [data, setCollections] = useState([]);
 
-    async function getCollections() {
-        const req = await fetch('/api/collections');
-        const collectionData = (await req.json());
-        setCollections(collectionData);
-    }
+    // async function getCollections() {
+    //     const req = await fetch('/api/collections');
+    //     const collectionData = (await req.json());
+    //     setCollections(collectionData);
+    // }
 
-    useEffect(() => {
-        getCollections();
-    }, [])
+    // useEffect(() => {
+    //     getCollections();
+    // }, [])
 
     return (
         <div className={styles.header}>
@@ -27,12 +27,12 @@ export default function Header () {
             </div>
             <div className={styles.navs}>
                 <Link className={styles.navlink} href="/all-photos" >View All Images</Link>
-                <div className={styles.navlink}>
+                {/* <div className={styles.navlink}>
                     <div class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Collections</div>
                     <ul class="dropdown-menu">
                         {data.map((d) => (<li><a class="dropdown-item" href={`/collections/${d.data.name}`}>{d.data.name}</a></li>))}
                     </ul>
-                </div>
+                </div> */}
             </div>
         </div>
     );

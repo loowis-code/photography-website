e5e1a9a4f8b7e8933595477667e77ab1dcaf2590
Lewis Inches <incheslewis@gmail.com>
2023-07-31 22:01:24 +0100

    responsive css for individual photo page

--- components/photo-body.js
+++ components/photo-body.js
@@ -14,18 +14,11 @@ export default function PhotoBody({data}) {
                 />
             </div> 
             <div className={styles.imageHeader}>
-                <div className="row">
-                    <p className={styles.imagetitle}>{(data.photo_data.location)? <p>{data.title}, {data.photo_data.location}</p> : null}</p>
-                    <p className={styles.imagedate}>{new Date(data.photo_data.date_taken).toLocaleDateString("en-GB",{year: 'numeric', month: 'long', day: 'numeric' })}</p>
-                </div>
+                <p className={styles.imagetitle}>{(data.photo_data.location)? <p>{data.title}, {data.photo_data.location}</p> : null}</p>
+                <p className={styles.imagedate}>{new Date(data.photo_data.date_taken).toLocaleDateString("en-GB",{year: 'numeric', month: 'short', day: 'numeric' })}</p>
             </div>
             <div className={styles.imageDetails}>
-                <div className="row">
-                    <div className={styles.imagesettings}>
-                        <p>Camera - {data.photo_data.camera}</p>
-                        <p>{(data.photo_data.film)? <p>Film - {data.photo_data.film}</p> : null}</p>
-                    </div>
-                </div>
+                <p className={styles.imageCamera}>Camera: {data.photo_data.camera}{(data.photo_data.film)? <p>Film: {data.photo_data.film}</p> : null}</p>
             </div>            
         </div>
     )

74129f2125c9362a43098c6648864019d8dbe2fc
Lewis Inches <incheslewis@gmail.com>
2023-08-14 00:26:46 +0100

    fix margins on photo body

--- components/photo-body.js
+++ components/photo-body.js
@@ -12,14 +12,15 @@ export default function PhotoBody({data}) {
                     height="400"
                     layout='responsive'                 
                 />
+                <div className={styles.imageHeader}>
+                    <p className={styles.imagetitle}>{(data.photo_data.location)? <p>{data.title}, {data.photo_data.location}</p> : null}</p>
+                    <p className={styles.imagedate}>{new Date(data.photo_data.date_taken).toLocaleDateString("en-GB",{year: 'numeric', month: 'short', day: 'numeric' })}</p>
+                </div>
+                <div className={styles.imageDetails}>
+                    <p className={styles.imageCamera}>Camera: {data.photo_data.camera}{(data.photo_data.film)? <p>Film: {data.photo_data.film}</p> : null}</p>
+                </div>    
             </div> 
-            <div className={styles.imageHeader}>
-                <p className={styles.imagetitle}>{(data.photo_data.location)? <p>{data.title}, {data.photo_data.location}</p> : null}</p>
-                <p className={styles.imagedate}>{new Date(data.photo_data.date_taken).toLocaleDateString("en-GB",{year: 'numeric', month: 'short', day: 'numeric' })}</p>
-            </div>
-            <div className={styles.imageDetails}>
-                <p className={styles.imageCamera}>Camera: {data.photo_data.camera}{(data.photo_data.film)? <p>Film: {data.photo_data.film}</p> : null}</p>
-            </div>            
+        
         </div>
     )
 }

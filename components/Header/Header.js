fd9c32f7aa94baa51c7ca12a87d819ba2ac64767
Lewis Inches <incheslewis@gmail.com>
2023-09-23 21:03:57 +0100

    fix pathname

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,14 +1,14 @@
 import Link from 'next/link'
 import styles from './Header.module.css'
-import { usePathname} from 'next/navigation';
-import { use } from 'react';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
-    let pathname = new RegExp();
-    if (usePathname() != "/") {
-        pathname = new RegExp(usePathname());
+    let pathname = usePathname()
+    let pathnameRegex
+    if (pathname != "/") {
+        pathnameRegex = new RegExp(pathname);
     } else {
-        pathname = new RegExp("homepage");
+        pathnameRegex = new RegExp("homepage");
     }
 
 
@@ -24,13 +24,13 @@ export default function Header() {
             </div>
             
             <div className={styles.navs}>
-                <Link className={pathname.test("/all-images") ? (styles.active) : styles.navlink} href="/all-images">
+                <Link className={pathnameRegex.test("/all-images") ? (styles.active) : styles.navlink} href="/all-images">
                     All Images
                 </Link>
-                <Link className={pathname.test("/collections") ? (styles.active) : styles.navlink} href="/collections">
+                <Link className={pathnameRegex.test("/collections") ? (styles.active) : styles.navlink} href="/collections">
                     Collections
                 </Link>
-                <Link className={pathname.test("/image-map") ? (styles.active) : styles.navlink} href="/image-map">
+                <Link className={pathnameRegex.test("/image-map") ? (styles.active) : styles.navlink} href="/image-map">
                     Image Map
                 </Link>
             </div>

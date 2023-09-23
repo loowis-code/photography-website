f7c9874556d1885175b797045abeb974185540f5
Lewis Inches <incheslewis@gmail.com>
2023-09-23 21:47:36 +0100

    add 404 page

---
+++ pages/404.js
@@ -0,0 +1,38 @@
+import Layout from '../components/Layout'
+import styles from './css-modules/four-oh-four.module.css'
+import Head from 'next/head'
+import ImageModal from '../components/ImageModal'
+import { useState, useEffect } from 'react'
+
+function custom404() {
+    const [photo, setPhoto] = useState([])
+
+    async function getAllImages() {
+        const req = await fetch('/api/getPhotos')
+        const photoData = await req.json()
+        const randomPhoto = photoData[Math.floor(Math.random() * photoData.length)]
+
+        setPhoto(randomPhoto)
+    }
+
+    useEffect(() => {
+        getAllImages()
+    }, [])
+
+    return (
+        <Layout>
+            <Head>
+                <title>404 | Lewis Inches Photography</title>
+            </Head>
+            <section className={styles.container}>
+                <h1 className={styles.heading}>This page doesn't exist :/</h1>
+                <div className={styles.photoContainer}>
+                <ImageModal data={photo} />
+                </div>
+                
+            </section>
+        </Layout>
+    )
+}
+
+export default custom404

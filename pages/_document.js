8eaa846228485f6003add895ce8df1c425622e96
Lewis Inches <incheslewis@gmail.com>
2025-02-26 23:34:18 +0000

    remove bootstrap

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,6 @@ export default function Document() {
     <Html>
       <Head>
         <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-            crossOrigin="anonymous"
-        ></link>
-        <link
             rel="stylesheet"
             href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
             integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="

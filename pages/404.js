4431bbfd778d5e945b0b10b8e98b31b6303df247
Lewis Inches <incheslewis@gmail.com>
2025-10-19 23:46:58 +0100

    fix 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,13 @@
 import Layout from '../components/Layout'
 import styles from './css-modules/four-oh-four.module.css'
 import Head from 'next/head'
-import ImageModal from '../components/ImageModal'
 import { useState, useEffect } from 'react'
 
 function Custom404() {
     const [photo, setPhoto] = useState([])
 
     async function getAllImages() {
-        const req = await fetch('/api/management/read/photos')
+        const req = await fetch('/api/admin/read/images')
         const photoData = await req.json()
         const randomPhoto =
             photoData[Math.floor(Math.random() * photoData.length)]
@@ -28,7 +27,13 @@ function Custom404() {
             <section className={styles.container}>
                 <h1 className={styles.heading}>This page does not exist :/</h1>
                 <div className={styles.photoContainer}>
-                    <ImageModal data={photo} />
+                    {photo && photo.url && (
+                        <img
+                            src={photo.url}
+                            alt={photo.title || 'Random Photo'}
+                            className={styles.photo}
+                        />
+                    )}
                 </div>
             </section>
         </Layout>
